aaf8525c472d1b9b98c7ca22a23410330b2505fd
korimse <korimse@gmail.com>
2021-02-14 18:25:44 +0900

    디비 에러 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import Customer from './components/Customer';
 import './App.css';
+import Customer from './components/Customer';
 import CustomerAdd from './components/CustomerAdd';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -14,22 +14,35 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 const styles = theme => ({
   root:{
     width:'100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX:"auto"
   },
   table:{
     minWidth:1080
   },
   progress:{
-    margin : theme.spacing.unit * 2
+    margin : theme.spacing(2)
   }
 });
 
 class App extends Component{
 
-  state = {
-    customer: "",
-    completed: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
+  }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
   }
 
   componentDidMount(){
@@ -50,48 +63,37 @@ class App extends Component{
     this.setState({completed:completed >= 100 ? 0 : completed + 1});
   }
 
-  render(){
-    const {classes} = this.props;
+  render() {
+    const { classes } = this.props;
     return (
-      <div>  
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-        {
-          this.state.customers ? this.state.customers.map(c=>{
-            return(
-              <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-              />
-              )
-            }) : 
-            <TableRow>
-            <TableCell colspan="6" align="center">
-              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-            </TableCell>
-          </TableRow>
-        }
-          </TableBody>
-        </Table>
-      </Paper>
-      <CustomerAdd></CustomerAdd>
-    </div>
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers ? this.state.customers.map(c => {
+                return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
+              }) :
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
+            </TableBody>
+          </Table>
+        </Paper>
+        <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
     );
   }
 }
--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import {post} from 'axios';
+import { post } from 'axios';
 
-class CustomerAdd extends React.Component{
-    constructor(props){
+class CustomerAdd extends React.Component {
+
+    constructor(props) {
         super(props);
         this.state = {
-            file:null,
-            userName:'',
-            birthday:'',
-            gender:'',
-            job:'',
-            fileName:''
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: ''
         }
     }
 
-    handleformsubmit = (e) => {
-        e.prventDefault()
+    handleFormSubmit = (e) => {
+        e.preventDefault()
         this.addCustomer()
-            .then((response)=>{
+            .then((response) => {
                 console.log(response.data);
-            });
+                this.props.stateRefresh();
+            })
+        this.setState({
+            file: null,
+            userName: '',
+            birthday: '',
+            gender: '',
+            job: '',
+            fileName: ''
+        })
     }
 
     handleFileChange = (e) => {
         this.setState({
-            file : e.target.files[0],
-            fileName : e.target.value
+            file: e.target.files[0],
+            fileName: e.target.value
         })
     }
 
@@ -39,13 +49,13 @@ class CustomerAdd extends React.Component{
         const url = '/api/customers';
         const formData = new FormData();
         formData.append('image', this.state.file);
-        formData.append('name', this.state.useName);
+        formData.append('name', this.state.userName);
         formData.append('birthday', this.state.birthday);
         formData.append('gender', this.state.gender);
         formData.append('job', this.state.job);
-        const config={
-            headers:{
-                'content-type':'multipart/form-data'
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
             }
         }
         return post(url, formData, config);
@@ -53,17 +63,18 @@ class CustomerAdd extends React.Component{
 
     render() {
         return (
-            <form onSubmit={this.handleformsubmit}>
+            <form onSubmit={this.handleFormSubmit}>
                 <h1>고객 추가</h1>
-                프로필 이미지 : <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}></input>
-                이름 : <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}></input>
-                생년월일 : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}></input>
-                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}></input>
-                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}></input>
+                프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+                이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
+                생년월일: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
                 <button type="submit">추가하기</button>
             </form>
         )
     }
+
 }
 
 export default CustomerAdd;
